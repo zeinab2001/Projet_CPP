ff72b78951b4c54cb469d6bbcf844494b7b5e7fd
achraf <achraf.nagmar@esprit.tn>
2022-04-07 01:38:02 -1100

    commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,25 @@
 #include "fournisseurs.h"
 #include <QMessageBox>
 
+#include <QPieSlice>
+#include <QPieSeries>
+#include <QtCharts/QChartView>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtCharts/QBarSeries>
+#include <QtCharts/QBarSet>
+#include <QtCharts/QLegend>
+#include <QtCharts>
+#include <QtCharts/QChartView>
+#include <QtCharts/QPieSeries>
+#include <QtCharts/QPieSlice>
+
+#include <QPrinter>
+#include <QtPrintSupport/QPrinter>
+#include <QPrintDialog>
+#include <QTextDocument>
+#include <QTextStream>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -108,4 +127,127 @@ void MainWindow::on_pushButton_chercher_clicked()
     QString rech_field = ui->lineEdit_nom_recherche->text();
     ui->tableViewAffichage->setModel(FRN.chercher(rech_field));
 
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    QString strStream;
+
+                         QTextStream out(&strStream);
+
+                         const int rowCount = ui->tableViewAffichage->model()->rowCount();
+                         const int columnCount = ui->tableViewAffichage->model()->columnCount();
+                         QString TT = QDateTime::currentDateTime().toString();
+                         out <<"<html>\n"
+                               "<head>\n"
+                                "<meta Content=\"Text/html; charset=Windows-1251\">\n"
+                             << "<title>ERP - COMmANDE LIST<title>\n "
+                             << "</head>\n"
+                             "<body bgcolor=#ffffff link=#5000A0>\n"
+                                "<h1 style=\"text-align: center;\"><strong> "+TT+"</strong></h1>"
+                                +"<img src=C:\\Users\\maato\\Desktop\\Nouveau%20dossier\\fahed.pdf />"
+                                "<h1 style=\"text-align: center;\"><strong> ****LISTE DES  licence commerciale **** </strong></h1>"
+
+
+                             "<table style=\"text-align: center; font-size: 20px;\" border=1>\n "
+                               "</br> </br>";
+                         // headers
+                         out << "<thead><tr bgcolor=#d6e5ff>";
+                         for (int column = 0; column < columnCount; column++)
+                             if (!ui->tableViewAffichage->isColumnHidden(column))
+                                 out << QString("<th>%1</th>").arg(ui->tableViewAffichage->model()->headerData(column, Qt::Horizontal).toString());
+                         out << "</tr></thead>\n";
+
+                         // data table
+                         for (int row = 0; row < rowCount; row++) {
+                             out << "<tr>";
+                             for (int column = 0; column < columnCount; column++) {
+                                 if (!ui->tableViewAffichage->isColumnHidden(column)) {
+                                     QString data =ui->tableViewAffichage->model()->data(ui->tableViewAffichage->model()->index(row, column)).toString().simplified();
+                                     out << QString("<td bkcolor=0>%1</td>").arg((!data.isEmpty()) ? data : QString("&nbsp;"));
+                                 }
+                             }
+                             out << "</tr>\n";
+                         }
+                         out <<  "</table>\n"
+                             "</body>\n"
+                             "</html>\n";
+
+                         QTextDocument *document = new QTextDocument();
+                         document->setHtml(strStream);
+
+                         QPrinter printer;
+
+                         QPrintDialog *dialog = new QPrintDialog(&printer, nullptr);
+                         if (dialog->exec() == QDialog::Accepted) {
+                             document->print(&printer);
+                         }
+
+                         delete document;
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    //ui->stackedWidget_2->setCurrentIndex(1);
+                QSqlQueryModel * model= new QSqlQueryModel();
+                model->setQuery("select * from fournisseurs where id_fournisseur < 199 ");
+                float taille=model->rowCount();
+                model->setQuery("select * from fournisseurs where id_fournisseur  between 199 and 299 ");
+                float taillee=model->rowCount();
+                model->setQuery("select * from fournisseurs where id_fournisseur >299 ");
+                float tailleee=model->rowCount();
+                float total=taille+taillee+tailleee;
+                QString a=QString("fournisseurs fidele . "+QString::number((taille*100)/total,'f',2)+"%" );
+                QString b=QString("fournisseur non fidele .  "+QString::number((taillee*100)/total,'f',2)+"%" );
+                QString c=QString("fournisseur en cas d urgence ."+QString::number((tailleee*100)/total,'f',2)+"%" );
+                QPieSeries *series = new QPieSeries();
+                series->append(a,taille);
+                series->append(b,taillee);
+                series->append(c,tailleee);
+                if (taille!=0)
+                {QPieSlice *slice = series->slices().at(0);
+                    slice->setLabelVisible();
+                    slice->setPen(QPen());}
+                if ( taillee!=0)
+                {
+                    // Add label, explode and define brush for 2nd slice
+                    QPieSlice *slice1 = series->slices().at(1);
+                    //slice1->setExploded();
+                    slice1->setLabelVisible();
+                }
+                if(tailleee!=0)
+                {
+                    // Add labels to rest of slices
+                    QPieSlice *slice2 = series->slices().at(2);
+                    //slice1->setExploded();
+                    slice2->setLabelVisible();
+                }
+                // Create the chart widget
+                QChart *chart = new QChart();
+                // Add data to chart with title and hide legend
+                chart->addSeries(series);
+                chart->setTitle("distribution fournisseurs :Nombre Des fournisseurs "+ QString::number(total));
+                chart->legend()->hide();
+                // Used to display the chart
+                QChartView *chartView = new QChartView(chart);
+                chartView->setRenderHint(QPainter::Antialiasing);
+                chartView->resize(1000,500);
+                chartView->show();
+}
+
+
+
+
+void MainWindow::on_pb_image_clicked()
+{
+    QFileDialog dialog(this);
+        dialog.setNameFilter(tr("Images (*.png *.xpm *.jpg)"));
+        dialog.setViewMode(QFileDialog::Detail);
+        QString fileName =QFileDialog::getOpenFileName(this, tr("Open Images"), "C:/Users/Achraf Nagmar/Desktop/ProjectFournisseurs", tr("Image Files (*.png *.jpg *.bmp)"));
+    //to select and show the picture
+        if (!fileName.isEmpty())
+        {
+            QImage image(fileName);
+                ui->label_pic->setPixmap(QPixmap::fromImage(image));
+        }
 }
